711ea952401f9bdec3d89c87dd0256c57edf920e
Cruz <A01738823@tec.mx>
2025-10-30 18:46:31 -0600

    Solution

---
+++ caritas/caritasApp.swift
@@ -0,0 +1,30 @@
+//
+//  caritasApp.swift
+//  caritas
+//
+//  Created by Cruz Yael Pérez González on 30/10/25.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+
+@main
+struct caritasApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
